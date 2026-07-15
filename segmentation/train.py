388028d3bbb8f623692d293a527711eb49285037
baiomania2021 <hokutomusou2009@yahoo.co.jp>
2026-07-15 15:43:21 +0900

    organize segmemtation directory

--- segmentation/train.py
+++ segmentation/train.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm import tqdm
 
 # 先ほど作成した model.py から Medical3DCNN をインポート！
-from model import Medical3DCNN
+from segmentation.model import Medical3DCNN
 
 # ==========================================
 # 1. 医療画像用のカスタムDatasetクラス
